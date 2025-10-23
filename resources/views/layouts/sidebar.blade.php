74a8461264673f7e7b41a02d0e0f6053c1164607
GalaxyFSRP1 <awarford3245619@gmail.com>
2025-10-23 18:31:20 -0400

    Atlas Hosting sidebar override

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,8 @@
+<aside class="sidebar">
+    <div class="brand">
+        <img src="{{ asset('logo.png') }}" alt="Atlas Hosting Logo">
+        <div class="brand-text">Atlas Hosting</div>
+    </div>
+    <!-- Sidebar navigation links -->
+    @include('partials.sidebar-links')
+</aside>
