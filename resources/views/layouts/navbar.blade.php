54917b047c6785ecb309a30b4bf3e6805a35a0bd
GalaxyFSRP1 <awarford3245619@gmail.com>
2025-10-23 18:31:31 -0400

    Atlas Hosting navbar override

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,7 @@
+<nav class="navbar">
+    <div class="navbar-brand">
+        <img src="{{ asset('favicon.ico') }}" width="24" height="24" alt="Atlas Icon">
+        Atlas Hosting Panel
+    </div>
+    @include('partials.nav-links')
+</nav>
