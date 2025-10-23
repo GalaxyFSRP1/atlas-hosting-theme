947ec7cc968267cb48603f9980763243e664caa8
GalaxyFSRP1 <awarford3245619@gmail.com>
2025-10-23 18:58:37 -0400

    Delete resources directory

--- resources/views/layouts/navbar.blade.php
+++
@@ -1,7 +0,0 @@
-<nav class="navbar">
-    <div class="navbar-brand">
-        <img src="{{ asset('favicon.ico') }}" width="24" height="24" alt="Atlas Icon">
-        Atlas Hosting Panel
-    </div>
-    @include('partials.nav-links')
-</nav>
--- resources/views/layouts/sidebar.blade.php
+++
@@ -1,8 +0,0 @@
-<aside class="sidebar">
-    <div class="brand">
-        <img src="{{ asset('logo.png') }}" alt="Atlas Hosting Logo">
-        <div class="brand-text">Atlas Hosting</div>
-    </div>
-    <!-- Sidebar navigation links -->
-    @include('partials.sidebar-links')
-</aside>
